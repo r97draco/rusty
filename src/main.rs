93ea48068381836366f9b23ce9edf2707745d16c
Jagroop Singh <sjagroop212@gmail.com>
2024-02-27 08:50:56 -0600

    + vectors

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,18 @@ fn main() {
     // auto_infer_datatype();
     // mut_data_types();'
     // control_flow();
-    functions();
+    // functions();
+    // collec_arrays();
+    collec_vectors();
 }
 
+// 1
 // Hello World
 fn hello_world() {
     println!("Hello, world!");
 }
 
+// 2
 // Console Input/Output
 fn io() {
     let stdin = io::stdin();
@@ -31,6 +35,7 @@ fn io() {
     print!("Hello {} !", name.trim());
 }
 
+// 3
 //Variables and Data Types
 // Here are the basic data types in C++ (64bit) and Rust
 // C++	            Rust
@@ -96,47 +101,48 @@ fn mut_data_types() {
     b += 100; // ok, since b is mutable
 }
 
+// 4
 // Control Flow
-fn control_flow(){
+fn control_flow() {
     let x = 1;
-    if x>0 {
+    if x > 0 {
         println!("Positive number");
-    } else if x<0 {
+    } else if x < 0 {
         println!("Negative number");
     } else {
         println!("Zero");
     }
 
     for i in 0..10 {
-        println!("{}*{}={}", i,i,i*i);
+        println!("{}*{}={}", i, i, i * i);
     }
 
     let mut t = 10;
-    while t<100 {
-        t+=t/2; 
+    while t < 100 {
+        t += t / 2;
     }
     println!("{}", t);
 
     // do while in c++
-    t= 0;
-    loop{
+    t = 0;
+    loop {
         println!("{}", t);
-        t+=2;
-        if !(t<=10){
+        t += 2;
+        if !(t <= 10) {
             break;
         }
     }
 
     let age = 15;
-    match age{
+    match age {
         0..=12 => println!("Child"),
         n @ 13..=19 => println!("Teenager at the age of {}", n),
-        n => println!("Adult at the age of {}", n)
+        n => println!("Adult at the age of {}", n),
     }
 }
 
-
-// Functions 
+// 5
+// Functions
 // The return keyword can also be removed if the semicolon at the end of the return statement is not present, which is similar to some languages such as Ruby or Erlang
 fn add(x: i32, y: i32) -> i32 {
     x + y
@@ -152,3 +158,70 @@ fn functions() {
     let z = add(x, y);
     print(z);
 }
+
+// 6
+// Collections
+
+// Arrays
+// There are two ways to declare an array in rust :
+// By specifying the initial value and the number of element of the array : [init_value; N] : this creates an array with N elements, each has the value of init_value
+// By listing all elements of the array [element1,element2,element3...], the elements are separated by the commas.
+struct Person {
+    name: &'static str,
+    age: i32,
+}
+
+fn collec_arrays() {
+    let mut arr = [0; 10];
+    for i in 0..10 {
+        arr[i] = i;
+    }
+
+    for x in &arr {
+        // same as : for x in arr.iter()
+        print!("{}", x);
+    }
+    println!("");
+
+    let persons = [
+        Person { name: "Alice", age: 20 },
+        Person { name: "Bob", age: 30 },
+        Person { name: "Charlie", age: 40 },
+        Person { name: "David", age: 50 },
+    ];
+
+    for perosn in &persons {
+        println!("{} is {} years old", perosn.name, perosn.age);
+    }
+}
+
+//Vectors
+// Vec::new()
+// vec![init_value;N]
+// vec![element1,element2,....]
+fn collec_vectors() {
+    let mut a: Vec<i32> = Vec::new();
+    let mut b: Vec<i32> = Vec::new();
+
+    a.push(1);
+    a.push(2);
+    a.push(3);
+
+    for x in &a {
+        // same as : for x in a.iter()
+        b.push(2 * x);
+    }
+
+    for (i, x) in a.iter().enumerate() {
+        println!("a[{}] = {}", i, x);
+    }
+
+    //The zip function joins two arrays into a new array with each element being a pair of elements from the two arrays. By using this technique, the code inside the loop does not need to check for index boundary but still remains safe, so the performance remain the same as C++ programs (C++ programs do not check index boundary by default and are considered unsafe)
+    for (x, y) in a.iter().zip(b.iter()) {
+        println!("2*{} {}", x, y);
+    }
+}
+
+//Map
+
+//Set
